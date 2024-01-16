0b6304ef749ccc745b37d5ba2db11caafbf27b0e
RafaelAmador16 <amadorcastellanorafaa@gmail.com>
2024-01-16 14:35:10 +0100

    clase 16

--- proyectos/00-introduccion/src/App.jsx
+++ proyectos/00-introduccion/src/App.jsx
@@ -2,8 +2,12 @@
 
 //Variables globales
 
+import ComponenteProps from "./components/Props/ComponenteProps";
+import UseEffectBasic from "./components/UseEffect/GitHubUserCardApi/UseEffectBasic";
+import ResizeListener from "./components/UseEffect/GitHubUserCardApi/ResizeListener";
 import RegistrarFormulario from "./components/UseState/RegistrarFormulario";
 import TodoListTailWind from "./components/UseState/TodoListTailWind";
+import GitHubUser from "./components/UseEffect/GitHubUserCardApi/GitHubUser";
 
 const App = () => {
 
@@ -11,8 +15,13 @@ const App = () => {
     <>
       <h1>Ejemplo basico de react</h1>
       <hr />
-      <RegistrarFormulario />
+      {/*}<RegistrarFormulario />
       <TodoListTailWind />
+      <ComponenteProps />
+      <UseEffectBasic />
+       <ResizeListener />
+  {*/}
+     <GitHubUser />
     </>
 
   )
